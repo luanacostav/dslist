6a982e2073c91dda9de619543690a7b6220ea889
luanacostav <costavasconcelosluana@gmail.com>
2025-08-22 09:55:14 -0300

    GET game by id

--- gamelist/gamelist/src/main/java/com/devluana/gamelist/controllers/GameController.java
+++ gamelist/gamelist/src/main/java/com/devluana/gamelist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.devluana.gamelist.controllers;
 
+import com.devluana.gamelist.dto.GameDTO;
 import com.devluana.gamelist.dto.GameMinDTO;
 import com.devluana.gamelist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,11 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        return gameService.findById(id);
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll() {
