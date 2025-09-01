012c6b595dfddb6d5d5fb3127c5eff2c5d3795b0
luanacostav <costavasconcelosluana@gmail.com>
2025-09-01 11:01:37 -0300

    Move games on list

--- gamelist/gamelist/src/main/java/com/devluana/gamelist/controllers/GameListController.java
+++ gamelist/gamelist/src/main/java/com/devluana/gamelist/controllers/GameListController.java
@@ -2,13 +2,11 @@ package com.devluana.gamelist.controllers;
 
 import com.devluana.gamelist.dto.GameListDTO;
 import com.devluana.gamelist.dto.GameMinDTO;
+import com.devluana.gamelist.dto.ReplacementDTO;
 import com.devluana.gamelist.services.GameListService;
 import com.devluana.gamelist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,5 +28,10 @@ public class GameListController {
     @GetMapping(value = "/{listId}/games")
     public List<GameMinDTO> findByList(@PathVariable Long listId) {
         return gameService.findByList(listId);
+    }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
     }
 }
--- gamelist/gamelist/src/main/java/com/devluana/gamelist/services/GameListService.java
+++ gamelist/gamelist/src/main/java/com/devluana/gamelist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.devluana.gamelist.services;
 
 import com.devluana.gamelist.dto.GameListDTO;
 import com.devluana.gamelist.entities.GameList;
+import com.devluana.gamelist.projections.GameMinProjection;
 import com.devluana.gamelist.repositories.GameListRepository;
+import com.devluana.gamelist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,10 +17,28 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(GameListDTO::new).toList();
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = Math.min(sourceIndex, destinationIndex);
+        int max = Math.max(sourceIndex, destinationIndex);
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 
 }
